9e457189af7604ff1425c0f1dec032f75329ada0
jonnyroutley <Jonathan.wei.liang@gmail.com>
2017-05-11 20:33:20 +0100

    made a separate css page for shows, made it fully responsive. Changed scales to vw instead of absolute values which is great.

--- radio.js
+++ radio.js
@@ -1,8 +1,13 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("dropdown-menu").classList.toggle("show");
+window.onload = function(){
+  var menuButton = document.getElementById("menu-button");
+
+  menuButton.onclick = function(event){
+      document.getElementById("dropdown-menu").classList.toggle("show");
+  }
 }
+
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
